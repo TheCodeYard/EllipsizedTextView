bbbda08f3f625327b62ca4aff7c6d989a5ed19d9
Georgios Savvidis <george.savvidis@thecodeyard.com>
2019-04-25 13:27:46 +0300

    Databinding example

--- app/src/main/java/com/thecodeyard/ellipsizedtextview/databinding/DatabindingActivity.kt
+++ app/src/main/java/com/thecodeyard/ellipsizedtextview/databinding/DatabindingActivity.kt
@@ -1,15 +1,25 @@
 package com.thecodeyard.ellipsizedtextview.databinding
 
+import android.databinding.ObservableField
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.thecodeyard.ellipsizedtextview.R
 
 
 class DatabindingActivity : AppCompatActivity() {
+    val longText = ObservableField<String>()
+    val ellipsisStart = ObservableField<String>()
+    val ellipsisMiddle = ObservableField<String>()
+    val ellipsisEnd = ObservableField<String>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_databinding)
         setTitle(R.string.title_databinding)
+
+        longText.set(getString(R.string.long_text))
+        ellipsisStart.set(getString(R.string.ellipsis_start))
+        ellipsisMiddle.set(getString(R.string.ellipsis_middle))
+        ellipsisEnd.set(getString(R.string.ellipsis_end))
     }
 }
